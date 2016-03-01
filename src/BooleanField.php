30f897f2b8c8c9cb32645fbfef0868aabc7c1917
Hugo Arribas <hugo.arribas@gmail.com>
2016-03-01 18:10:50 +0100

    Bug fixed with BooleanField if value = 0 (false)

--- src/BooleanField.php
+++ src/BooleanField.php
@@ -4,4 +4,17 @@ namespace Phonf;
 
 class BooleanField extends Field {
 
+    public function getDBValue() {
+        if (!is_null($this->value)) {
+            if ($this->value == 0) {
+                $statement = "0";
+            } else {
+                $statement = $this->value;
+            }
+        } else {
+            $statement = "null";
+        }
+        return $statement;
+    }
+
 }
