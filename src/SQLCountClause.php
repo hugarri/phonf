7dbaf664a72e329906d4b832a8adbfb2a924bf3e
Hugo Arribas <hugo.arribas@gmail.com>
2016-05-31 09:17:15 +0200

    Improvements in SQLCountClause class

--- src/SQLCountClause.php
+++ src/SQLCountClause.php
@@ -7,18 +7,35 @@ class SQLCountClause {
     private $field;
     private $aliasField;
 
-    public function setField($field) {
+    public function __construct(Field $field) {
+        $this->field = $field;
+        $this->aliasField = new NumericField($field->getDatabase(), "Count");
+    }
+
+    /**
+     * @param Field $field
+     */
+    public function setField(Field $field) {
         $this->field = $field;
     }
 
+    /**
+     * @return Field
+     */
     public function getField() {
         return $this->field;
     }
 
-    public function setAliasField($aliasField) {
+    /**
+     * @param Field $aliasField
+     */
+    public function setAliasField(Field $aliasField) {
         $this->aliasField = $aliasField;
     }
 
+    /**
+     * @return Field
+     */
     public function getAliasField() {
         return $this->aliasField;
     }
