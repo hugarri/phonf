e8f62499af175797bcb7f38a20054f6ab9b3127a
Hugo Arribas <hugo.arribas@gmail.com>
2016-09-14 17:39:36 +0200

    When StringField has "null" (or any other capitalized version of null) as value, its DBValue is null

--- src/StringField.php
+++ src/StringField.php
@@ -6,7 +6,7 @@ class StringField extends Field {
 
     public function getDBValue() {
 
-        if (!is_null($this->value)) {
+        if (!is_null($this->value) and strtoupper($this->value) !== "NULL") {
             $statement = '"' . str_replace('"','\"',$this->value) . '"';
         } else {
             $statement = "null";
